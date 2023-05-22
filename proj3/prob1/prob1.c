2cf840345da9553ae8df64c4163ba68c0bac4908
biggyu <sgkevinlee@cau.ac.kr>
2023-05-23 00:46:57 +0900

    proj3 prob1 return value modified

--- proj3/prob1/prob1.c
+++ proj3/prob1/prob1.c
@@ -2,10 +2,14 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
-#include <time.h>
+// #include <time.h>
 #include <string.h>
 #include <omp.h>
 const int NUM_END = 200000;
+typedef struct value {
+	double exc_time;
+	int total_prime;
+} values;
 bool isPrime(int x) {
 	if (x <= 1)
 		return false;
@@ -15,10 +19,11 @@ bool isPrime(int x) {
 	}
 	return true;
 }
-int getPrime(int schedule_type, int thread_num) {
+values getPrime(int schedule_type, int thread_num) {
+	values v;
 	int sum = 0, i;
-	printf("%d %d\n", schedule_type, thread_num);
-	clock_t start_time = clock();
+	// printf("%d %d\n", schedule_type, thread_num);
+	double start_time = omp_get_wtime();
 #pragma omp parallel num_threads(thread_num)
 	if (schedule_type == 1) { // static w/ default chunk
 #pragma omp for private(i) schedule(static)
@@ -57,29 +62,23 @@ int getPrime(int schedule_type, int thread_num) {
 			}
 		}
 	}
-	clock_t end_time = clock();
-	printf("%dms time elapsed in ", end_time - start_time);
-	return sum;
+	double end_time = omp_get_wtime();
+	v.exc_time = (end_time - start_time) * 1000;
+	v.total_prime = sum;
+	return v;
 }
 int main(int argc, char *argv[]) {
-	if (argc == 2) {
-		int thread_nums[] = {1, 2, 4, 6, 8, 10, 12, 14, 16};
-		int schedule_type = atoi(argv[1]);
-		for (int i = 0; i < sizeof(thread_nums) / sizeof(thread_nums[0]); i++) {
-			int result = getPrime(schedule_type, thread_nums[i]);
-			printf("%d threads, %s scheduling type, chunk size %s envinronment\n", thread_nums[i], schedule_type % 2 == 0 ? "dynamic" : "static", schedule_type < 3 ? "default" : "10");
-			printf("1... %d has %d prime #\n", NUM_END, result);
-		}
-	} else if (argc == 3) {
+	if (argc == 3) {
 		int thread_num = atoi(argv[2]), schedule_type = atoi(argv[1]);
-		clock_t start_time = clock();
-		int result = getPrime(schedule_type, thread_num);
-		clock_t end_time = clock();
-		printf("%d thread w/ %s scheduling type chunk size %s\n", thread_num, schedule_type % 2 == 0 ? "dynamic" : "static", schedule_type < 3 ? "default" : "10");
-		printf("1... %d has %d prime #\n", NUM_END, result);
+		double start_time = omp_get_wtime();
+		values result = getPrime(schedule_type, thread_num);
+		double end_time = omp_get_wtime();
+		printf("%lfms time elapsed in %d thread w/ %s scheduling type chunk size %s\n", result.exc_time, thread_num,
+		schedule_type % 2 == 0 ? "dynamic" : "static", schedule_type < 3 ? "default" : "10");
+		printf("1... %d has %d prime #\n", NUM_END, result.total_prime);
 		// getPrime(atoi(argv[1]), atoi(argv[2]));
-	} else {
-		int thread_nums[] = {1, 2, 4, 6, 8, 10, 12, 14, 16};
+	} else if(argc == 1) {
+		int thread_nums[] = {1, 2, 4, 6, 8, 10, 12, 14, 16, 32};
 		FILE *fp;
 		fp = fopen("prob1_result.csv", "w+");
 		fprintf(fp, ",");
@@ -94,18 +93,18 @@ int main(int argc, char *argv[]) {
 			fprintf(fp, i % 2 == 0 ? "static," : "dynamic,");
 			fprintf(fp, i < 2 ? "default," : "10,");
 			for(int j = 0; j < sizeof(thread_nums) / sizeof(thread_nums[0]); j++) {
-				clock_t start_time = clock();
-				int result = getPrime(i + 1, thread_nums[j]);
-				clock_t end_time = clock();
+				values result = getPrime(i + 1, thread_nums[j]);
 				char tmp[20];
-				itoa(end_time - start_time, tmp, 10);
+				sprintf(tmp, "%.2f", result.exc_time);
 				fprintf(fp, strcat(tmp, "ms,"));
+				printf("%d primes\n", result.total_prime);
 			}
 			fprintf(fp, "\n");
-		}
-		// printf("prob1.c needs at least 1 input parameters(prob1 scheduling_type# (#_of_thread))\n");
-		// printf("scheduling_type#:\n1:static w/ default chunk\n");
-		// printf("2:dynamic w/ default chunk\n3:static w/ chunk 10\n4:dynamic w/ chunk 10\n");
-		// printf("\n#_of_thread: 1, 2, 4, 6, 8, 10, 12, 14, 16");
+		};
+	} else {
+		printf("prob1.c needs 2 input parameters: prob1 scheduling_type# #_of_thread\n");
+		printf("scheduling_type#:\n1:static w/ default chunk\n");
+		printf("2:dynamic w/ default chunk\n3:static w/ chunk 10\n4:dynamic w/ chunk 10\n");
+		printf("\n#_of_thread: 1, 2, 4, 6, 8, 10, 12, 14, 16");
 	}
 }
