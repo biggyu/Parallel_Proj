0a5c150888ecf312b938167acaf2fbd20a9193d7
biggyu <sgkevinlee@cau.ac.kr>
2023-05-31 22:37:07 +0900

    proj4 prob1 openmp update

--- proj4/problem1/openmp_ray.cpp
+++ proj4/problem1/openmp_ray.cpp
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <omp.h>
 #include <math.h>
-
+#include <time.h>
 #define SPHERE_NUM 20
 #define rnd( x ) (x * rand() / RAND_MAX)
 #define INF 2e10f
@@ -26,15 +26,12 @@ typedef struct Sphere {
 } Sphere;
 double openmp_ray(Sphere* s, unsigned char* ptr) {
 	int i, j, k;
-	// float rgb[4] = {0, 0, 0, 1}, maxz = -INF;
 	double start_time = omp_get_wtime();
-	// printf("%d\n", DIM);
 #pragma omp parallel num_threads(thread_num) private(i, j, k) shared(ptr)
 #pragma omp for collapse(2)
 	for(i = 0; i < DIM; i++) {
 		for(j = 0; j < DIM; j++) {
-			float ox = (i - DIM / 2), oy = (j - DIM / 2), maxz = -INF;
-			float rgb[4] = {0, 0, 0, 1};
+			float ox = (i - DIM / 2), oy = (j - DIM / 2), maxz = -INF, rgb[4] = {0, 0, 0, 1};
 			for(k = 0; k < SPHERE_NUM; k++) {
 				float n, t = s[k].hit(ox, oy, &n);
 				if (t > maxz) {
@@ -46,30 +43,12 @@ double openmp_ray(Sphere* s, unsigned char* ptr) {
 				}
 			}
 			
+			int offset = i + j * DIM;
 			for (k = 0; k < 4; k++) {
-				int offset = i + j * DIM;
 				ptr[offset * 4 + k] = (int)(rgb[k] * 255);
-				// ptr[offset * 4 + 0] = (int)(r * 255);
-				// ptr[offset * 4 + 1] = (int)(g * 255);
-				// ptr[offset * 4 + 2] = (int)(b * 255);
-				// ptr[offset * 4 + 3] = 255;
 			}
 		}
 	}
-// #pragma omp barrier
-// #pragma omp for collapse(3)
-// 	for(i = 0; i < DIM; i++) {
-// 		for(j = 0; j < DIM; j++) {
-// 			for (k = 0; k < 4; k++) {
-// 				int offset = i + j * DIM;
-// 				ptr[offset * 4 + k] = (int)(rgb[k] * 255);
-// 				// ptr[offset * 4 + 0] = (int)(r * 255);
-// 				// ptr[offset * 4 + 1] = (int)(g * 255);
-// 				// ptr[offset * 4 + 2] = (int)(b * 255);
-// 				// ptr[offset * 4 + 3] = 255;
-// 			}
-// 		}
-// 	}
 	return omp_get_wtime() - start_time;
 }
 void ppm_write(unsigned char* bitmap, int xdim,int ydim, FILE* fp) {
@@ -88,8 +67,9 @@ void ppm_write(unsigned char* bitmap, int xdim,int ydim, FILE* fp) {
 int main(int argc, char *argv[]) {
 	if (argc == 2) {
 		thread_num = atoi(argv[1]);
-		FILE* rp = fopen("result.ppm","w");
+		FILE* rp = fopen("result_openmp.ppm", "w");
 		Sphere *spheres = (Sphere *)malloc(sizeof(Sphere) * SPHERE_NUM);
+		srand((unsigned int)time(NULL));
 		for (int i = 0; i < SPHERE_NUM; i++) {
 			spheres[i].r = rnd(1.0f);
 			spheres[i].g = rnd(1.0f);
@@ -113,7 +93,7 @@ int main(int argc, char *argv[]) {
 		free(spheres);
 
 		printf("Openmp (%d threads) ray tracing: %lf sec\n", thread_num, exc_time);
-		printf("[result.ppm] generated\n");
+		printf("[openmp_result.ppm] generated\n");
 	} else {
 		// printf("prob1.c needs 1 input parameter: ./a #_of_thread\n");
 		// printf("\n#_of_thread: 1, 2, 4, 6, 8, 10, 12, 14, 16, 32\n");
@@ -138,10 +118,11 @@ int main(int argc, char *argv[]) {
 		}
 		fprintf(fp, "\n,execution time,");
 		for(int i = 0; i < sizeof(thread_nums) / sizeof(thread_nums[0]); i++) {
-			char tmp[20];
+			char tmp[20], res[40] = "result_";
 			itoa(thread_nums[i], tmp, 10);
-			strcat(tmp, "_result.ppm");
-			rp = fopen(tmp,"w");
+			strcat(res, tmp);
+			strcat(res, "_openmp.ppm");
+			rp = fopen(res, "w");
 			bitmap = (unsigned char *)malloc(sizeof(unsigned char) * DIM * DIM * 4);
 			thread_num = thread_nums[i];
 			double exc_time = openmp_ray(spheres, bitmap);
@@ -161,5 +142,5 @@ int main(int argc, char *argv[]) {
 }
 
 
-//g++ -fopenmp -o a openmp_ray.cpp
-//./a result.ppm
+//g++ -fopenmp -o openmp_ray openmp_ray.cpp
+//./openmp_ray 4
